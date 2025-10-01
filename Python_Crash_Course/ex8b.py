e796f4d2d917bfd812713f5a59a32c2d36f20595
Franco Lugo <81472752+FranCode1@users.noreply.github.com>
2025-10-01 17:41:07 -0300

    Delete Python_Crash_Course directory

--- Python_Crash_Course/ex8b.py
+++
@@ -1,9 +0,0 @@
-from ex8a import Car
-from ex8a import ElectricCar as EC
-
-my_new_car = Car('audi', 'a4', 2024)
-print(my_new_car.get_descriptive_name())
-my_new_car.odometer_reading = 23
-my_new_car.read_odometer()
-
-my_leaf = EC('nissan', 'leaf', 2024)
